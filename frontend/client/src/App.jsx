5e3a8b1c578db2c5a0d6c2f189854baa01549e5a
gitonup789 <j0224697@gmail.com>
2023-11-08 15:21:04 -0500

    switching to main

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Test from './components/Test'
 import Registration from './components/Registration'
-import RoundTwo from './components/RoundTwo'
+// import RoundTwo from './components/RoundTwo'
 import FinalRound from './components/FinalRound'
 import Homepage from './Homepage'
+// import Round from './components/Round'
+import Roundthree from './components/Roundthree'
 
 const App = () => {
   return (
     <>
-    <RoundTwo />
+    {/* <RoundTwo /> */}
+    {/* <Round /> */}
+    <Roundthree />
     </>
   )
 }
--- frontend/client/src/components/Roundthree.jsx
+++ frontend/client/src/components/Roundthree.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import './Round.css'
-function Round() {
+
+function Roundthree() {
   const [team1Points, setTeam1Points] = useState(0);
   const [team2Points, setTeam2Points] = useState(0);
   const [team1Answer, setTeam1Answer] = useState('');
   const [team2Answer, setTeam2Answer] = useState('');
-  const [team1X, setTeam1X]=useState(0)
-  const [team2X, setTeam2X]=useState(0)
   const gameQuestions = ["Name a food that comes in a box"];
   const gameAnswersWithScores = {
     "macaroni and cheese": 20,
@@ -18,26 +17,22 @@ function Round() {
   };
   const handleAnswer = (answer, team) => {
     if (gameQuestions.length > 0) {
-      const score = gameAnswersWithScores[answer.toLowerCase];
+      const score = gameAnswersWithScores[answer.toLowerCase()];
       if (score !== undefined) {
         if (team === 1) {
           setTeam1Points(team1Points + score);
-          console.log(setTeam1Points(team1Points + score))
         } else if (team === 2) {
           setTeam2Points(team2Points + score);
-          console.log(setTeam2Points(team2Points + score))
         }
       }
     }
   };
-  const handleInput=()=>{
-  }
   return (
-    <div>
+    <div className='gamepage'>
          <h1 className='header'>Game Question</h1>
       <p>{gameQuestions[0]}</p>
-      <p className='teams'>Team 1's total points: {team1Points}</p>
-      <p className='teams'>Team 2's total points: {team2Points}</p>
+      <p>Team 1's total points: {team1Points}</p>
+      <p>Team 2's total points: {team2Points}</p>
       <input
         type="text"
         placeholder="Team 1's Answer"
@@ -55,4 +50,5 @@ function Round() {
     </div>
   );
 }
-export default Round;
+
+export default Roundthree;
