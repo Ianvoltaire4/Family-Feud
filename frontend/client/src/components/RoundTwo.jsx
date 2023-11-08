35e21384fa72e31155be44ee66db0c567e8c86f9
Deron <fambroderon@gmail.com>
2023-11-08 14:53:05 -0500

    switching to pull in main

--- frontend/client/src/components/RoundTwo.jsx
+++ frontend/client/src/components/RoundTwo.jsx
@@ -3,11 +3,13 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { Link } from "react-router-dom";
-import CoinFlip from "./CoinFlip";
+import CoinFlip from "./CoinFlip"
 import Flipcoin from "./CoinFlip";
+
+
 
 const RoundTwo = () => {
   //inputs for both teams
@@ -48,7 +50,7 @@ const RoundTwo = () => {
   //bootstrap
   const [smShow, setSmShow] = useState(false);
   const [lgShow, setLgShow] = useState(false);
-  const [show, setShow] = useState(false);
+  const [show, setShow]=useState(false)
   const values = [true];
   const [fullscreen, setFullscreen] = useState(true);
   function handleShow(breakpoint) {
@@ -113,11 +115,12 @@ const RoundTwo = () => {
     setCurrentTeam(currentTeam === "Team One" ? "Team Two" : "Team One");
     setOtherTeam(otherTeam === "Team Two" ? "Team One" : "Team Two");
   };
-
+  
   // const gameQuestions = [
-  //     "Name a food that comes in a box.",
+  //     "Name a food that comes in a box.", 
   //     "what 9 + 10"
   // ]
+    
 
   //   const gameAnsweres = [
   //     "macaroni and cheese",
@@ -128,28 +131,9 @@ const RoundTwo = () => {
   //     "pizza"
   //   ]
 
-  const gameQuestions = ["Name a food that comes in a box"];
-  const gameAnswersWithScores = {
-    "macaroni and cheese": 20,
-    crackers: 15,
-    "hamburger helper": 10,
-    donuts: 25,
-    popcorn: 30,
-    pizza: 10,
-  };
 
-  const handleAnswer = (answer) => {
-    if (gameQuestions.length > 0) {
-      const score = gameAnswersWithScores[answer.toLowerCase()];
-      if (score !== undefined) {
-        if (currentTeam === 1) {
-          setTeamOneScore(teamOneScore + score);
-        } else if (currentTeam === 2) {
-          setTeamTwoScore(teamTwoScore + score);
-        }
-      }
-    }
-  };
+
+  
 
   // const questionData = [
   //   { answer: "macarone and cheese", score: 10 },
@@ -159,7 +143,8 @@ const RoundTwo = () => {
   //   { answer: "popcorn", score: 2 },
   //   { answer: "pizza", score: 0 },
   // ];
-
+  
+  
   // const handleDataPopulation = () => {
   //   if (teamOneInput || teamTwoInput === 'pizza'){
   //     setTeamOneScore(answerOne)
@@ -168,18 +153,20 @@ const RoundTwo = () => {
   //   }
   // };
 
+
+
+
   useEffect(() => {
     if (!showButton) {
       const hideButton = setTimeout(() => {
-        startAndStop();
         setShowButton(false);
       }, 5000);
       return () => clearTimeout(hideButton);
     }
-
+    startAndStop;
     setLgShow(true);
   }, [showButton]);
-
+  
   const populateData = () => {
     setQuestion(q);
     setAnswerOne(a1);
@@ -206,9 +193,11 @@ const RoundTwo = () => {
 
   const reset = () => {
     setTime(300);
-    startAndStop();
+    startAndStop()
   };
-  const handleAnwers = () => {};
+const handleAnwers=()=>{
+
+}
   //FUNCTION I MADE FOR HANDLING THE DATA FOR A WRONG ANSWWER, NEED TO CHANGE AFTER WE CAN POPULATE THE DATA
   const teamOneWrongAnswer = () => {
     // Compares the teamOneInput with the correct answer (answerOne)
@@ -236,37 +225,44 @@ const RoundTwo = () => {
     }
   };
 
-  const [isRound1QuestionActive, setIsRound1QuestionActive] = useState(false);
+  const [isRound1QuestionActive, setIsRound1QuestionActive] = useState(false)
 
+
+   
+      
   //   if (gameQuestions[0]){
   //   for (let i = 0; i < gameAnsweres.length; i ++){
   //     if (team1Answer === "pizza"){
   //       setteam1Points (team1points + 10)
   //     }
-
+  
   //     if (team1Answer === "crackers"){
   //       setteam1Points (team1points + 36)
   //     }
   //   }
   // }
 
-  // {
-  //   q: '',
-  //   a1: '',
-  //   a2: '',
-  //   a3: '',
-  //   a4: '',
-  //   a5: '',
-  //   a6: '',
-  //   a7: '',
-  //   s1: ,
-  //   s2: ,
-  //   s3: ,
-  //   s5: ,
-  //   s4: ,
-  //   s6: ,
-  //   s7:
-  // }
+    
+    // {
+    //   q: '',
+    //   a1: '',
+    //   a2: '',
+    //   a3: '',
+    //   a4: '',
+    //   a5: '',
+    //   a6: '',
+    //   a7: '',
+    //   s1: ,
+    //   s2: ,
+    //   s3: ,
+    //   s5: ,
+    //   s4: ,
+    //   s6: ,
+    //   s7:
+    // }
+  ;
+
+
   const assignPoints = () => {
     //make if else statements for giving points based on what answer the user gets right
   };
@@ -281,10 +277,12 @@ const RoundTwo = () => {
     console.log(populateData(randomObject));
   };
 
-  const handleScoreAssignment = () => {
-    if (teamOneInput || teamTwoInput === "pizza") {
+  const handleScoreAssignment=()=>{
+    if(teamOneInput || teamTwoInput === 'pizza' ){
+      
     }
-  };
+  }
+
 
   const handleRoundOver = () => {
     let boardCleared = false;
@@ -342,105 +340,91 @@ const RoundTwo = () => {
   //   if(coinFlip == "heads"){
   //     return(
   //       <>
-
+  
   //       </>
   //     )
   //   }else{
   //     return(
   //       <>
-
+  
   //       </>
   //     )
   //   }
   // }
 
+
   return (
     <>
-      <>
-        {values.map((v, idx) => (
-          <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
-            Fullscreen
-            {typeof v === "string" && `below ${v.split("-")[0]}`}
-          </Button>
-        ))}
-        <Modal
-          show={show}
-          fullscreen={fullscreen}
-          onHide={() => setShow(false)}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Modal body content</Modal.Body>
-        </Modal>
-      </>
+       <>
+      {values.map((v, idx) => (
+        <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
+          Fullscreen
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Modal body content</Modal.Body>
+      </Modal>
+    </>
 
-      {/* ==================================================== */}
+    {/* ==================================================== */}
       <Button onClick={() => setLgShow(true)}></Button>
       <Modal
         size="lg"
         show={lgShow}
         onHide={() => setLgShow(false)}
         aria-labelledby="example-modal-sizes-title-lg"
-      >
+        >
         <Modal.Header closeButton>
           <Modal.Title id="example-modal-sizes-title-lg">
-            GAME RULES:{" "}
-            <h6 id="rules">
-              dgoidnvknoeinhjvcgvjhbjnlbhvjgchfvbnknvkjkdfvjklmdfjhnvkldmnjbhfjivnkfbdliv;jlkndfbhlvnlkdfjbvndflkjbhlvndklmcnfjbvhldfnclkmvnjblhdfknkcmlvnjbhdflcnvklm/njbhfdvimknjedfbjkmvnjdfj.nvmkndjf
-              jnkvmndj.fcknmvdfnj vnkmdfnj vnmkdfn v.ndfmk/cv nfndkm vndcm/k
-              df.mcfdnvnkfcvmdfk cmvmfc/ fnmck nckm vfmmdc,mcdm
-              k.fdcmkcilibvcvbnm;nbuiyvutcrytvubnimnubiyvtcvbv
-            </h6>
-            {!isRunning && showButton && (
+            GAME RULES: <h6 id="rules">dgoidnvknoeinhjvcgvjhbjnlbhvjgchfvbnknvkjkdfvjklmdfjhnvkldmnjbhfjivnkfbdliv;jlkndfbhlvnlkdfjbvndflkjbhlvndklmcnfjbvhldfnclkmvnjblhdfknkcmlvnjbhdflcnvklm/njbhfdvimknjedfbjkmvnjdfj.nvmkndjf jnkvmndj.fcknmvdfnj vnkmdfnj vnmkdfn v.ndfmk/cv nfndkm vndcm/k df.mcfdnvnkfcvmdfk cmvmfc/ fnmck nckm vfmmdc,mcdm k.fdcmkcilibvcvbnm;nbuiyvutcrytvubnimnubiyvtcvbv</h6>
+        {!isRunning && showButton && (
+          <div>
+            {/* <div>{gameQuestions[0]}</div> */}
+            <div>
               <div>
-                {/* <div>{gameQuestions[0]}</div> */}
-                <div>
-                  <div>
-                    <input
-                      type="text"
-                      placeholder="ENTER TEAM NAME"
-                      value={teamName}
-                      onChange={handleInputChange}
-                    />
-                    <button onClick={handleTeamName}>
-                      {" "}
-                      Save name as heads
-                    </button>
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      placeholder="ENTER TEAM NAME"
-                      value={otherTeamName}
-                      onChange={handleOtherInputChange}
-                    />
-                    <button onClick={handleOtherTeamName}>
-                      {" "}
-                      Save name as tails
-                    </button>
-                  </div>
-                </div>
+                <input
+                  type="text"
+                  placeholder="ENTER TEAM NAME"
+                  value={teamName}
+                  onChange={handleInputChange}
+                  />
+                <button onClick={handleTeamName}> Save name as heads</button>
               </div>
-            )}
+              <div>
+                <input
+                  type="text"
+                  placeholder="ENTER TEAM NAME"
+                  value={otherTeamName}
+                  onChange={handleOtherInputChange}
+                  />
+                <button onClick={handleOtherTeamName}> Save name as tails</button>
+              </div>
+            </div>
+          </div>
+        )}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Flipcoin />
+        <Flipcoin />
           <div>
-            Heads: {currentTeam}
-            <br />
-            Tails: {otherTeam}
-          </div>
-          <button id="start" onClick={handleStart}>
-            Start Game
-          </button>
+        Heads: {currentTeam}
+        <br />
+        Tails: {otherTeam}
+        </div>
+            <button id="start" onClick={handleStart}>
+              Start Game
+
+            </button>
         </Modal.Body>
       </Modal>
 
-      {/* ============================================================================================================== */}
+    {/* ============================================================================================================== */}
 
-      {/* <div>
+    {/* <div>
   <h1>{question}</h1>
   <ul>
     {questionData.map((item, index) => (
@@ -451,7 +435,8 @@ const RoundTwo = () => {
   </ul>
 </div> */}
 
-      {/* ============================================================================================================== */}
+    {/* ============================================================================================================== */}
+
 
       <div id="board">
         <div>{question}</div>
@@ -476,66 +461,58 @@ const RoundTwo = () => {
           </div>
         </div>
       </div>
-      {
-        // time === 0 ||
-        wrongAnswersTeamOne === maxWrongAnswers ||
-        wrongAnswersTeamTwo === maxWrongAnswers ? (
-          <>
-            <Button onClick={() => setSmShow(true)} className="me-2">
-              Small modal
-            </Button>
-            <Button onClick={() => setLgShow(true)}>Large modal</Button>
-            <Modal
-              size="sm"
-              show={smShow}
-              onHide={() => setSmShow(false)}
-              aria-labelledby="example-modal-sizes-title-sm"
-            >
-              <Modal.Header closeButton>
-                <Modal.Title id="example-modal-sizes-title-sm">
-                  <h3>
-                    DING DING DING DING, TIMES UP {handleTeamSwitch},{" "}
-                    {currentTeam} you have lost you're turn
-                  </h3>
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <h3>
-                  On to the next team! {otherTeam} you're up! All you need to do
-                  to take the points for this round, is guess one more item on
-                  the board. If its not there then {currentTeam} take this
-                  round!!!
-                </h3>
-                <button>{currentTeam}'s Turn</button>
+{
+// time === 0 || 
+wrongAnswersTeamOne === maxWrongAnswers || wrongAnswersTeamTwo === maxWrongAnswers ? (
+        <>
+      <Button onClick={() => setSmShow(true)} className="me-2">
+        Small modal
+      </Button>
+      <Button onClick={() => setLgShow(true)}>Large modal</Button>
+      <Modal
+        size="sm"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+          <h3>DING DING DING DING, TIMES UP {handleTeamSwitch}, {currentTeam} you have lost you're turn</h3>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h3>
+            On to the next team! {otherTeam} you're up! All you need to do to
+            take the points for this round, is guess one more item on the board.
+            If its not there then {currentTeam} take this round!!!
+          </h3>
+          <button >{currentTeam}'s Turn</button>
 
-                {/* <button id="switch-teams" onClick={handleTeamSwitch}>
+          {/* <button id="switch-teams" onClick={handleTeamSwitch}>
             switch
           </button> */}
-              </Modal.Body>
-            </Modal>
-          </>
-        ) : (
-          <>
-            {sec.toString().padStart(2, "0")}:
-            {millisec.toString().padStart(2, "0")}
-          </>
-        )
-      }
+        </Modal.Body>
+      </Modal>
+        </>
+
+      ) : (
+        <>
+          {sec.toString().padStart(2, "0")}:
+          {millisec.toString().padStart(2, "0")}
+        </>
+      )}
 
       <div>
         {wrongAnswersTeamOne < maxWrongAnswers && (
           <div>
-            {currentTeam}:
-            <input id="currentTeam" type="text" onChange={(e) => seTeamOneInput(e.target.value)} />
-            <button onClick={handleAnswer(teamOneInput)}>enter</button>
-            </div>
-           
+            {currentTeam}: <input id="currentTeam" type="text" onChange={setTeamOneInput} />
+            <button onClick={teamOneWrongAnswer}>enter</button>
+          </div>
         )}
         {wrongAnswersTeamTwo < maxWrongAnswers && (
           <div>
-            {otherTeam}:{" "}
-            <input id="currentTeam" type="text" onChange={(e) => setTeamTwoInput(e.target.value)} />
-            <button onClick={handleAnswer(teamTwoInput)}>Enter</button>
+            {otherTeam}: <input id="otherTeam" type="text" onChange={setTeamTwoInput} />
+            <button onClick={teamTwoWrongAnswer}>Enter</button>
           </div>
         )}
       </div>
@@ -544,3 +521,4 @@ const RoundTwo = () => {
 };
 
 export default RoundTwo;
+
