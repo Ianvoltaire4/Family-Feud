cecd41e0262a2ab9a3d376c9c2ddedbc3534fa8c
gitonup789 <j0224697@gmail.com>
2023-11-08 19:53:42 -0500

    switch to main

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Registration from './components/Registration'
 import Roundthree from './components/Roundthree'
-import RoundTwo from './components/RoundTwo'
+// import RoundTwo from './components/RoundTwo'
 
 
 const App = () => {
   return (
     <>
     <Roundthree />
-    <RoundTwo />
+    {/* <RoundTwo /> */}
     <Registration />
     </>
   )
